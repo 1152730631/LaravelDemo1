b8d4ac16ea6fd5ec2341b93d5327bd7cc15950ab
mrLifang <1152730631@qq.com>
2017-06-28 09:10:41 +0000

    页面样式优化

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,17 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="class" src="">
+            <a>
+                推翻帝国暴政!!! 实现自由交友
+            </a>
+        </small>
+
+        <nav>
+            <ul>
+                <li><a href="/about">关于</a></li>
+            </ul>
+        </nav>
+
+    </footer>
+</div>
